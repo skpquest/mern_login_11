5cc045ec1953ecbd85902aa2f3ee54531159b752
Harshal  Pagar <72097667+harshpagar@users.noreply.github.com>
2022-07-15 13:05:39 +0530

    add

--- mern-auth-backend/routes/auth.js
+++ mern-auth-backend/routes/auth.js
@@ -137,69 +137,69 @@ router.post(
 );
 
 //Upload File
-const DIR = "./public/";
+// const DIR = "./public/";
 
-const storage = multer.diskStorage({
-	destination: (req, file, cb) => {
-		cb(null, DIR);
-	},
-	filename: (req, file, cb) => {
-		const fileName = file.originalname.toLowerCase().split(" ").join("-");
-		cb(null, uuidv4() + "-" + fileName);
-	},
-});
+// const storage = multer.diskStorage({
+// 	destination: (req, file, cb) => {
+// 		cb(null, DIR);
+// 	},
+// 	filename: (req, file, cb) => {
+// 		const fileName = file.originalname.toLowerCase().split(" ").join("-");
+// 		cb(null, uuidv4() + "-" + fileName);
+// 	},
+// });
 
-var upload = multer({
-	storage: storage,
-	fileFilter: (req, file, cb) => {
-		if (
-			file.mimetype == "image/png" ||
-			file.mimetype == "image/jpg" ||
-			file.mimetype == "image/jpeg"
-		) {
-			cb(null, true);
-		} else {
-			cb(null, false);
-			return cb(new Error("Only .png, .jpg and .jpeg format allowed!"));
-		}
-	},
-});
+// var upload = multer({
+// 	storage: storage,
+// 	fileFilter: (req, file, cb) => {
+// 		if (
+// 			file.mimetype == "image/png" ||
+// 			file.mimetype == "image/jpg" ||
+// 			file.mimetype == "image/jpeg"
+// 		) {
+// 			cb(null, true);
+// 		} else {
+// 			cb(null, false);
+// 			return cb(new Error("Only .png, .jpg and .jpeg format allowed!"));
+// 		}
+// 	},
+// });
 
 //@route   POST /users/uploadfile
 //@desc    Avatar Upload File
 //@access  Public
-router.post(
-	"/uploadfile",
-	auth,
-	[upload.single("avatar")],
+// router.post(
+// 	"/uploadfile",
+// 	auth,
+// 	[upload.single("avatar")],
 
-	async (req, res) => {
-		try {
-			const url = req.protocol + "://" + req.get("host");
+// 	async (req, res) => {
+// 		try {
+// 			const url = req.protocol + "://" + req.get("host");
 
-			const user = await User.findOne({ _id: req.user.id });
+// 			const user = await User.findOne({ _id: req.user.id });
 
-			const deletepicture = user.avatar.split("/");
-			try {
-				fs.unlinkSync("public/" + deletepicture[4]);
-			} catch (err) {
-				console.log(err);
-			}
-			const response = await User.update(
-				{ _id: req.user.id },
-				{
-					$set: {
-						avatar: url + "/public/" + req.file.filename,
-					},
-				}
-			);
-			console.log(response);
-			return res.status(200).send();
-		} catch (err) {
-			console.error(err.message);
-			return res.status(500).send(message.SERVER_ERROR);
-		}
-	}
-);
+// 			const deletepicture = user.avatar.split("/");
+// 			try {
+// 				fs.unlinkSync("public/" + deletepicture[4]);
+// 			} catch (err) {
+// 				console.log(err);
+// 			}
+// 			const response = await User.update(
+// 				{ _id: req.user.id },
+// 				{
+// 					$set: {
+// 						avatar: url + "/public/" + req.file.filename,
+// 					},
+// 				}
+// 			);
+// 			console.log(response);
+// 			return res.status(200).send();
+// 		} catch (err) {
+// 			console.error(err.message);
+// 			return res.status(500).send(message.SERVER_ERROR);
+// 		}
+// 	}
+// );
 
 module.exports = router;
